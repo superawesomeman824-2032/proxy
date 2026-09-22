f78c7e8f1c963cbf316cb6b2d394fcad6e33a3c8
superawesomeman824-2032 <superawesomeman824@gmail.com>
2026-09-21 20:03:22 -0500

    uv.config.js

---
+++ uv.config.js
@@ -0,0 +1,11 @@
+self.__uv$config = {
+    prefix: '/uv/service/',
+    bare: 'https://tomp.app/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.handler.js',
+    client: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.client.js',
+    bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.sw.js',
+};
