c977860d3455d74a2761710a4fb2be39d405c494
superawesomeman824-2032 <superawesomeman824@gmail.com>
2026-09-22 20:27:33 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,11 +1,16 @@
+const pathSegments = location.pathname.split('/').filter(Boolean);
+const repoSubpath = (location.hostname.includes('github.io') && pathSegments.length > 0) 
+  ? '/' + pathSegments[0] + '/' 
+  : '/';
+
 self.__uv$config = {
-    prefix: './uv/service/',
+    prefix: repoSubpath + 'uv/service/',
     bare: 'https://bare-server-y5ch.onrender.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.handler.js',
     client: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.client.js',
     bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.bundle.js',
-    config: './uv.config.js',
-    sw: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.sw.js',
+    config: repoSubpath + 'uv.config.js',
+    sw: repoSubpath + 'sw.js',
 };
